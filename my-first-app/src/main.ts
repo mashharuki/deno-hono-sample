6666ed1f241b4c18177fd92bc17d0e0df374c815
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-10-20 10:56:33 +0900

    修正

--- my-first-app/src/main.ts
+++ my-first-app/src/main.ts
@@ -1,5 +1,29 @@
-import { Hono } from "hono";
-import { findAllUsers, findUserById } from "./util.ts";
+import { Hono, validator } from "hono";
+import {
+  addUser,
+  badRequest,
+  deleteUser,
+  findAllUsers,
+  findUserById,
+  isPostUserRequest,
+  isPutUserRequest,
+  notFound,
+  updateUser,
+} from "./util.ts";
+
+// insert initial data
+await addUser({
+  name: "Harmon",
+  age: 25,
+});
+await addUser({
+  name: "Wheeler",
+  age: 26,
+});
+await addUser({
+  name: "Parks",
+  age: 27,
+});
 
 const app = new Hono();
 
@@ -24,8 +48,39 @@ app.get(
   const id = c.req.param("id");
   // ユーザー情報を取得する。
   const value = await findUserById(id);
-  return value ? c.json({ ...value, id }) : c.json("notFound", 404);
-});
+  return value ? c.json({ ...value, id }) : c.json(notFound, 404);
+}).post( // ユーザー追加APIメソッド
+  "/users",
+  validator("json", (value, c) => {
+    return isPostUserRequest(value) ? value : c.json(badRequest, 400);
+  }),
+  async (c) => {
+    const { name, age } = c.req.valid("json");
+    const { result, id } = await addUser({ name, age });
+    if (result.ok) {
+      return c.json({ name, age, id }, 201);
+    }
+    return c.json(internalServerError, 500);
+  },
+).put( // ユーザー更新APIメソッド
+  "/users/:id",
+  validator("json", (value, c) => {
+    return isPutUserRequest(value) ? value : c.json(badRequest, 400);
+  }),
+  async (c) => {
+    const id = c.req.param("id");
+    const value = c.req.valid("json");
+    const isUpdated = await updateUser(id, value);
+    return isUpdated ? c.json(null) : c.json(badRequest, 400);
+  },
+).delete( // ユーザー削除APIメソッド
+  "/users/:id",
+  async (c) => {
+    const id = c.req.param("id");
+    const result = await deleteUser(id);
+    return result ? c.json(null) : c.json(badRequest, 400);
+  },
+);
 
 Deno.serve(app.fetch);
 
--- my-first-app/src/util.ts
+++ my-first-app/src/util.ts
@@ -11,9 +11,42 @@ try {
 
 const kv = await Deno.openKv("kv.sqlite");
 
+const ulid = monotonicFactory();
+
 export type User = {
   name: string;
   age: number;
+};
+
+type PostUserRequest = User;
+
+type Error = {
+  message: string;
+};
+
+type PutUserRequest = Partial<User>;
+
+export const isPostUserRequest = (val: unknown): val is PostUserRequest => {
+  return typeof val === "object" && !!val && "name" in val && "age" in val &&
+    typeof val.name === "string" && typeof val.age === "number";
+};
+
+export const badRequest: Error = {
+  message: "Invalid Request",
+} as const;
+
+export const notFound: Error = {
+  message: "Not Found",
+} as const;
+
+export const internalServerError: Error = {
+  message: "Internal Server Error",
+} as const;
+
+export const isPutUserRequest = (val: unknown): val is PutUserRequest => {
+  return typeof val === "object" && !!val &&
+    (!("name" in val) || typeof val.name === "string") &&
+    (!("age" in val) || typeof val.age === "number");
 };
 
 /**
@@ -39,4 +72,51 @@ export const findAllUsers = async () => {
 export const findUserById = async (id: string): Promise<User | null> => {
   const { value } = await kv.get<User>(["users", "id", id]);
   return value;
+};
+
+/**
+ * ユーザーを追加する
+ * @param user ユーザー情報
+ * @returns ユーザー情報
+ */
+export const addUser = async (user: User) => {
+  const id = ulid();
+  const primaryKey = ["users", "id", id];
+  return {
+    result: await kv.atomic().check({ key: primaryKey, versionstamp: null })
+      .set(primaryKey, user).commit(),
+    id,
+  };
+};
+
+/**
+ * ユーザーを更新する
+ * @param id ユーザーのID
+ * @param value 更新するユーザー情報
+ * @returns 更新結果
+ */
+export const updateUser = async (id: string, value: Partial<User>) => {
+  const user = await findUserById(id);
+  if (!user) {
+    return false;
+  }
+  const key = ["users", "id", id];
+  // ユーザー情報を更新する
+  const { ok } = await kv.set(key, { ...user, ...value });
+  return ok;
+};
+
+/**
+ * ユーザーを削除する
+ * @param id ユーザーのID
+ * @returns 削除結果
+ */
+export const deleteUser = async (id: string) => {
+  const user = await findUserById(id);
+  if (!user) {
+    return false;
+  }
+  const key = ["users", "id", id];
+  await kv.delete(key);
+  return true;
 };
